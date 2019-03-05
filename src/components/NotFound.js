e882a7030f872cab30f5701d805db3e7af9623fd
stephanie-phuong-khanh <stephaniekdoan@gmail.com>
2019-03-04 19:55:19 -0800

    not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../styles/notfound.css';
+import wave from '../styles/vectors/notfound-wave.png';
+import logo from '../styles/svg/logo.svg';
 
 class NotFound extends React.Component {
     render() {
       return(
-        <div>error page not found</div>
+        <div className="page">
+          <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet"></link>
+          <img className="wave" src={wave} alt="wave" />
+          <img className="logo" src={logo} alt="logo" />
+          <div className="oops">Oops, the page you're looking for does not exist.</div>
+          <div className="uncharted">These are uncharted waters.</div>
+          <div className="return">Still lost? Return home here.</div>
+        </div>
       )
     }
 }
--- src/components/ProfPic.js
+++ src/components/ProfPic.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group'; // ES6
-import { Button } from "react-bootstrap"
+//import { Link } from 'react-router-dom';
+//import { CSSTransitionGroup } from 'react-transition-group'; // ES6
+//import { Button } from "react-bootstrap"
 import '../styles/transitions.css';
 
 class ProfPic extends React.Component
